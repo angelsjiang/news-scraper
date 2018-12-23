daf1c4fa3caab1e32f76ef0f99f1b63e1e90e3cd
angeljiang0927@gmail.com <angeljiang0927@gmail.com>
2018-12-22 21:29:57 -0800

    modal works

--- public/app.js
+++ public/app.js
@@ -3,31 +3,7 @@ $.getJSON("/articles", function(data) {
 });
 
 $(document).on("click", ".list-group-item.list-group-item-action", function() {
-    alert("clicked!");
-
-    $(".modal").append(`
-        <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalCenterTitle">Modal title</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                    </div>
-                    <div class="modal-body">
-                    ...
-                    </div>
-                    <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    `)
-
-    // $(".modal").show();
+    
 });
 
 $(document).on("click", ".scrape-news", function(event) {
@@ -117,7 +93,7 @@ var getArticles = (data) => {
         }
 
         $(".list-group").append(`
-            <div class="list-group-item list-group-item-action flex-column align-items-start">
+            <div class="list-group-item list-group-item-action flex-column align-items-start" data-toggle="modal" data-target="#exampleModalCenter">
                 <div class="d-flex w-100 justify-content-between">
                     <h5 class="mb-1">${data[i].title}</h5>
                 </div>
