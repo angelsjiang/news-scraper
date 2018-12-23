59d3b51f80149803c446558833da6e63d8c6334b
angeljiang0927@gmail.com <angeljiang0927@gmail.com>
2018-12-22 20:48:21 -0800

    buttons all work

--- public/app.js
+++ public/app.js
@@ -1,18 +1,6 @@
 $.getJSON("/articles", function(data) {
-    console.log(data);
-    for (var i = 0; i < data.length; i++) {
-        $(".list-group").append(`
-            <div class="list-group-item list-group-item-action flex-column align-items-start">
-                <div class="d-flex w-100 justify-content-between">
-                    <h5 class="mb-1">${data[i].title}</h5>
-                </div>
-                <p class="mb-1">${data[i].summary}</p>
-                <small>${data[i].time}</small><br>
-                <button type="button" class="btn btn-warning save-btn" data-article=${data[i]._id}>Save Article</button>
-            </div>
-        `)
-    };
-})
+    getArticles(data);
+});
 
 $(document).on("click", ".list-group-item", function() {
     // alert("clicked!");
@@ -69,13 +57,27 @@ $(document).on("click", ".clear-news", function(event) {
 $(document).on("click", ".saved-news", function(event) {
     event.preventDefault();
 
-    alert("Yoyoyo!");
+    // alert("Yoyoyo!");
 
     $.ajax({
         url: "/articles/saved",
         type: "get"
-    }).then(function(result) {
-        console.log(result);
+    }).then(function(data) {
+
+        $(".list-group").empty();
+
+        for (var i = 0; i < data.length; i++) {
+            $(".list-group").append(`
+                <div class="list-group-item list-group-item-action flex-column align-items-start">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h5 class="mb-1">${data[i].title}</h5>
+                    </div>
+                    <p class="mb-1">${data[i].summary}</p>
+                    <small>${data[i].time}</small><br>
+                    <button type="button" class="btn btn-warning save-btn" data-article=${data[i]._id}>Save Article</button>
+                </div>
+            `)
+        };
     });
 });
 
@@ -94,3 +96,31 @@ $(document).on("click", ".save-btn", function(event) {
         console.log(result);
     });
 });
+
+
+$(document).on("click", ".home-btn", function() {
+    $.getJSON("/articles", function(data) {
+        getArticles(data);
+    });
+});
+
+
+// re-usable functions
+var getArticles = (data) => {
+
+    $(".list-group").empty();
+
+    console.log(data);
+    for (var i = 0; i < data.length; i++) {
+        $(".list-group").append(`
+            <div class="list-group-item list-group-item-action flex-column align-items-start">
+                <div class="d-flex w-100 justify-content-between">
+                    <h5 class="mb-1">${data[i].title}</h5>
+                </div>
+                <p class="mb-1">${data[i].summary}</p>
+                <small>${data[i].time}</small><br>
+                <button type="button" class="btn btn-warning save-btn" data-article=${data[i]._id}>Save Article</button>
+            </div>
+        `)
+    };
+};
